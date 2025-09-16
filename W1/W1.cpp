142ea9a44d0358aa6ad68533c7097b2ba6607455
Ryno11007 <rvakulich001@student.butte.edu>
2025-09-16 20:23:35 +0000

    lab 9/16

---
+++ W1/W1.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main() {
+    int x = 1;
+    bool b = true;
+    // boolean value
+    std::printf("%i", b);
+    // booleans are numbers, huzzah
+    std::printf("%i", x);
+    std::printf("hello world");
+   return 0;
+}
