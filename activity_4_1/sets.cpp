b339777121a855f5885ed8055202070a8aa037ab
Ryno11007 <rvakulich001@student.butte.edu>
2025-09-25 20:05:27 +0000

    guyigg

--- activity_4_1/sets.cpp
+++ activity_4_1/sets.cpp
@@ -22,15 +22,15 @@ int main () {
 
     std::vector<int> potatoe = {'6' , '4'};
 
-    std::printf("%i\n,", potatoe.at(1));
+    std::printf("%i\n,", potatoe.at(0));
 
     std::vector<int> butte = {'5' , '2'};
 
     std::printf("%i\n,", butte.at(0));
 
-    std::vector<int> red = {'2' , '1'};
+    std::vector<int> red = {'4' , '1'};
 
-    std::printf("%i\n,", red.at(1));
+    std::printf("%i\n,", red.at(0));
 
     return 0;
 }
---
+++ c++_func/func.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+
+
+    int my_sum(int x, int y) { //define a function name: my_sum, output: int, inputs: int, int
+        return x + y;           // describes the output
+    }
+
+    //anytime you use a function you must call it in the form <name>(<inputs>)
+    
+    int main() {
+
+        std::printf("%d\n", my_sum(80,3)); //
+    return 0;
+}
+
+
+
+
+
+    
+
+
+
+
